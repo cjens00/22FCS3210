666226f1ebed3f5bf3c14b2d5557e0ac4bbd46dc
cjens <>
2022-12-12 15:36:05 -0700

    Update for project 4.\nAdded VS Code workspace .json file.

--- prg_04_elections_sim/src/elections.go
+++ prg_04_elections_sim/src/elections.go
@@ -1,40 +1,41 @@
 /*
  * CS3210 - Principles of Programming Languages - Fall 2022
  * Instructor: Thyago Mota
- * Description: elections simulation with coroutines
- * Student Name(s): 
+ * Description: Elections simulation with coroutines
+ * Student Name(s): Cameron Jensen
  */
 
- package main
+package main
 
- import (
-	 "fmt"
-	 "math/rand"
-	 "sync"
-	 "time"
- )
- 
- const PROPOSALS  = 3
- const VOTERS     = 1000000
- 
- var wg sync.WaitGroup
- 
+import "sync"
+
+// "fmt"
+// "math/rand"
+// "sync"
+// "time"
+
+const PROPOSALS = 3
+const VOTERS = 1000000
+
+var wg sync.WaitGroup
+
 // TODO #1: create a ballot and randomly assign 0 (no) or 1 (yes) to each proposal; then sleep for up to 10s (also random); at the end of the loop, write the ballot to the (given) ballot_box channel
- func vote(ballot_box chan<- [PROPOSALS]int) {
-	 
- }
- 
- // TODO #2: return a tally of the results as a percent of 1s (yes) that was given to each proposal
- func tally_results(ballot_box <-chan [PROPOSALS]int) [PROPOSALS]float64 {
-	 
- }
- 
- func main() {
+func vote(ballot_box chan<- [PROPOSALS]int) {
+
+}
+
+// TODO #2: return a tally of the results as a percent of 1s (yes) that was given to each proposal
+func tally_results(ballot_box <-chan [PROPOSALS]int) [PROPOSALS]float64 {
+	println("Tally Results")
+	return [3]float64{1.0, 1.0, 1.0}
+}
+
+func main() {
 
 	// TODO #3: time when the election started
 
 	// TODO #4: create a ballot_box
- 
+
 	// TODO #4 create VOTERS goroutines
 
 	// TODO #5: wait for all goroutines to end
@@ -43,4 +44,4 @@
 
 	// TODO #7: show how long the election took in seconds
 
- }
+}
