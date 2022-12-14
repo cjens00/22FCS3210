8aebc52fd1867a92fb612c6b6fe24d0a55167505
cjens <>
2022-12-13 21:16:01 -0700

    Lua Elections nearing completion

--- prg_04_elections_sim/src/elections.lua
+++ prg_04_elections_sim/src/elections.lua
@@ -2,93 +2,120 @@
 -- Instructor: Thyago Mota
 -- Description: elections simulation with coroutines
 -- Student Name(s): Cameron Jensen
---[[
-    One Coroutine = One Voter Voting
-    (~ and a partridge in a pear tree ~)
-    (Happy Holidays '22!)
-]] --
-
-OS_WINDOWS = "windows"
-OS_LINUX = "linux"
-OS_MACOS = "macos"
-
--- Helper function to determine OS
--- https://stackoverflow.com/a/30960054/18372340
-function determine_os()
-    local BinaryFormat = package.cpath:match("%p[\\|/]?%p(%a+)")
-    if BinaryFormat == "dll" then
-        function os.name()
-            return OS_WINDOWS
-        end
-    elseif BinaryFormat == "so" then
-        function os.name()
-            return OS_LINUX
-        end
-    elseif BinaryFormat == "dylib" then
-        function os.name()
-            return OS_MACOS
-        end
-    end
-    BinaryFormat = nil
-end
-
--- Multi-platform sleep function (tested on Linux/WSL-Ubuntu)
--- Code from http://lua-users.org/wiki/SleepFunction
-function sleep(numSeconds)
-    local os = determine_os()
-    if os == OS_WINDOWS then
-        os.execute("timeout " .. tonumber(numSeconds))
-    elseif os == OS_LINUX or os == OS_MACOS then
-        local reqPOSIX = require("posix")
-        posix.sleep(numSeconds)
-        reqPOSIX = nil
-    end
-    os = nil
-end
-
 PROPOSALS = 3
 VOTERS = 1000000
 
---- Returns a random choice.
-function _get_random_choice(max)
-    local max = max or 100100
-    local seed = os.time() + math.random(max) % (2 ^ 1024 - 1);
-    math.randomseed(seed);
-    local rand_span = 1100100;
-    local random_value = math.random(rand_span)
-    if random_value * 2 >= rand_span then
-        max, seed, random_value, rand_span = nil, nil, nil, nil
-        return 1
-    end
-    max, seed, random_value, rand_span = nil, nil, nil, nil
-    return 0
-end
-
--- TODO #1:  then begin a timed loop of up to 10s (also random); 
--- TODO #1:  at each iteration the function should yield; 
--- TODO #1:  at the end of the loop, insert the ballot in the (given) ballot_box.
 function vote(ballot_box)
-    ballot = {}
-    for i = 1, #PROPOSALS + 1 do
-        ballot[i] = _get_random_choice()
+    local ballot = {}
+    for i = 1, PROPOSALS do
+        table.insert(ballot, i, (math.random() >= 0.500))
     end
-    ballot_box[#ballot_box + 1] = ballot
+    local startTime = os.clock()
+    local voteTime = math.random(1, 10)
+    local timeRemaining = voteTime - (os.clock() - startTime)
+    while timeRemaining > 0 do
+        coroutine.yield(timeRemaining)
+        timeRemaining = voteTime - (os.clock() - startTime)
+    end
+    table.insert(ballot_box, #ballot_box + 1, ballot)
 end
 
---- Returns a tally of the results as a percent of ones given to each proposal.
----@param ballot_box any
 function tally_results(ballot_box)
+    for i = 1, #ballot_box do
+        ballot_box[i] = tonumber(VOTERS / ballot_box[i])
+    end
+    return ballot_box -- is this already a reference? check.
 end
 
---- All other program logic begins in and resides here.
-function main()
-    -- TODO #3: Time when the election started
-    -- TODO #4: Create a ballot_box
-    -- TODO #4: Create an array of "vote" coroutines
-    -- TODO #5: Schedule the coroutines so each has a fair chance of running until completion
-    -- TODO #6: Display the results
-    -- TODO #7: Show how long the election took in seconds
+function schedule_voters(voters)
+    -- Max number of voters allowed at once
+    local voter_capacity = 10000
 
+    -- Partition voters into groups 
+    -- of (max) size voter_capacity
+    local voter_groups = {}
+    local num_groups = (VOTERS / voter_capacity)
+    if VOTERS % voter_capacity > 0 then
+        num_groups = num_groups + 1
+    end
+    for i = 1, num_groups do
+        local from = i * voter_capacity
+        local to = math.min((i * voter_capacity) + voter_capacity, #voters)
+        voter_groups[i] = table.unpack(voters, from, to)
+    end
+
+    -- Get rid of duplicate (and unpartitioned) voters
+    voters = nil
+
+    -- Define the procedure for each group of voters
+    local sort_frequency = 10
+    local sort_by_time_remaining = function(ascending)
+        return function()
+            if ascending then
+            else
+            end
+        end
+    end
+    local start_voter_group = function(group_id)
+        local this_group = voter_groups[group_id]
+        for voter in this_group do
+            voter = {
+                voter = voter,
+                timeRem = 0
+            }
+        end
+        while #this_group > 0 do
+            for i = 1, #this_group do
+                if coroutine.status(this_group[i]) ~= "dead" then
+                    this_group[i].timeRem = coroutine.resume((this_group[i]))
+                else
+                    table.remove(this_group, i)
+                end
+                if i % (#this_group / sort_frequency) == 0 then
+                    table.sort(this_group, sort_by_time_remaining(true))
+                end
+            end
+        end
+    end
+
+    -- Begin the group voting procedure, one group at a time
+    for i = 1, #voter_groups do
+        start_voter_group(group_id)
+    end
+end
+
+function begin_election()
+    -- Time when the election started
+    local startTime = os.clock()
+
+    -- Create a ballot_box
+    local ballot_box = {
+        [1] = 0,
+        [2] = 0,
+        [3] = 0
+    }
+
+    -- Create an array of "vote" coroutines
+    local voters = {}
+    for i = 1, VOTERS do
+        table.insert(voters, i, coroutine.create(vote(ballot_box)))
+    end
+
+    -- Schedule the coroutines so each has a 
+    -- fair chance of running until completion
+    schedule_voters(voters)
+
+    -- Show how long the election took 
+    -- in seconds and display the results
+    local results = tally_results(ballot_box)
+    local finishTime = os.clock() - startTime
+    local fStrResults = string.format("")
+    print(fStrResults)
+end
+
+-- Main below this line --
+function main()
+    begin_election()
 end
 
 main()
